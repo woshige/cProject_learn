54b7464994c746cf9437cc0a64801adf4039e393
sunlichao <sunlichao@qianxin.com>
2020-03-11 20:02:12 +0800

    'add'

---
+++ day_01/cmake-build-debug/CMakeFiles/day_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day_01.dir/main.c.obj"
+  "day_01.pdb"
+  "day_01.exe"
+  "day_01.exe.manifest"
+  "libday_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/day_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
