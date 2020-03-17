294968ef110ad3c4b4561af6302ead8c0275bdc6
sunlichao <sunlichao@qianxin.com>
2020-03-17 09:15:53 +0800

    'add'

---
+++ day_08/struct/test_struct.c
@@ -0,0 +1,4 @@
+//
+// Created by Administrator on 2020/3/16 0016.
+//
+
---
+++ day_08/struct/test_struct.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 2020/3/16 0016.
+//
+
+#ifndef DAY_08_TEST_STRUCT_H
+#define DAY_08_TEST_STRUCT_H
+
+#endif //DAY_08_TEST_STRUCT_H
